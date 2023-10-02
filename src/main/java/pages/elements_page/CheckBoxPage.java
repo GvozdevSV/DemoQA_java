dca81ee5c5e825c85894c0efcb020309991671cb
GvozdevSV <agasfen82@yandex.ru>
2023-10-02 20:17:19 +0400

    add scrollToElement

--- src/main/java/pages/elements_page/CheckBoxPage.java
+++ src/main/java/pages/elements_page/CheckBoxPage.java
@@ -2,7 +2,10 @@ package pages.elements_page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import pages.pages_base.BasePage;
+
+import java.util.List;
 
 public class CheckBoxPage extends BasePage {
     public CheckBoxPage(WebDriver driver) {
@@ -10,6 +13,7 @@ public class CheckBoxPage extends BasePage {
     }
     //locators
     private final By expandAllButton = By.cssSelector("button[title=\"Expand all\"]");
+    private final By checkboxes = By.cssSelector("span[class=\"rct-checkbox\"]");
 
 
 
@@ -18,8 +22,13 @@ public class CheckBoxPage extends BasePage {
         return this;
 
     }
-    //public clickRandomChecbox(){
+    public clickRandomCheckbox(){
+        List<WebElement> allCheckboxes = driver.findElements(checkboxes);
+        int count = 20;
+        do {
+            WebElement item = allCheckboxes[(Math.random() * 15) + 1];
+        }
 
-    //}
+    }
 
 }
